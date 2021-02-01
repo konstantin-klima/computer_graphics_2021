5b140a90124cf86f975c7eb8d88de3368267d252
Ognjen Popovic <rentorious@gmail.com>
2021-02-01 18:34:46 +0100

    ConcaveCollider createTriangleMesh refactor

--- include/ConcaveCollider.h
+++ include/ConcaveCollider.h
@@ -33,52 +33,35 @@ private:
     rp3d::TriangleMesh *createTriangleMesh(std::vector<Mesh> meshes, rp3d::PhysicsCommon &physicsCommon) {
         auto triangleMesh = physicsCommon.createTriangleMesh();
 
-        unsigned long meshLen = meshes.size();
+        for (const Mesh &mesh : meshes) {
+            mVertices.emplace_back();
+            mNormals.emplace_back();
+            mIndices.emplace_back();
 
-        int i = 0;
-        while (i < meshLen) {
             // Load vertex and normal coordinates into the member vector
-            std::vector<float> tmpVert;
-            std::vector<float> tmpNorm;
-            for (auto vertex : meshes[i].vertices) {
-                tmpVert.push_back(vertex.Position.x);
-                tmpVert.push_back(vertex.Position.y);
-                tmpVert.push_back(vertex.Position.z);
+            for (auto vertex : mesh.vertices) {
+                mVertices.back().push_back(vertex.Position.x);
+                mVertices.back().push_back(vertex.Position.y);
+                mVertices.back().push_back(vertex.Position.z);
 
-                tmpNorm.push_back(vertex.Normal.x);
-                tmpNorm.push_back(vertex.Normal.y);
-                tmpNorm.push_back(vertex.Normal.z);
+                mNormals.back().push_back(vertex.Normal.x);
+                mNormals.back().push_back(vertex.Normal.y);
+                mNormals.back().push_back(vertex.Normal.z);
             }
-            mVertices.push_back(tmpVert);
-            mNormals.push_back(tmpNorm);
 
             // Load indices into the member vector
-            std::vector<int> tmpIndi;
-            for (auto index : meshes[i].indices) {
-                tmpIndi.push_back(index);
+            for (auto index : mesh.indices) {
+                mIndices.back().push_back(index);
             }
-
-            mIndices.push_back(tmpIndi);
-
-            std::cout << tmpVert[0] << std::endl;
-            std:
-            cout << tmpNorm[0] << std::endl;
-            std::cout << tmpIndi[0] << std::endl;
-
-            std::cout << mVertices.back()[0] << std::endl;
-            std::cout << mNormals.back()[0] << std::endl;
-            std::cout << mIndices.back()[0] << std::endl;
-
-            fflush(stdout);
 
             // rp3d representation of vertex array for this mesh
             auto triangleArray = new rp3d::TriangleVertexArray(
-                    meshes[i].vertices.size(),
+                    mesh.vertices.size(),
                     (const void *) &mVertices.back()[0],
                     3 * sizeof(float),
                     (const void *) &mNormals.back()[0],
                     3 * sizeof(float),
-                    meshes[i].numFaces,
+                    mesh.numFaces,
                     (const void *) &mIndices.back()[0],
                     3 * sizeof(int),
                     rp3d::TriangleVertexArray::VertexDataType::VERTEX_FLOAT_TYPE,
@@ -88,7 +71,6 @@ private:
 
 //            add this as a subpart for the triangle mesh
             triangleMesh->addSubpart(triangleArray);
-            ++i;
         }
         return triangleMesh;
     }
