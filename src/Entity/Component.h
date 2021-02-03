4ea30b0368877b4d4c160a93493022a618856b60
Konstantin <konstantin.klima@live.com>
2021-02-03 20:38:32 +0100

    bugfix: Fix constructors for LightComponent

--- src/Entity/Component.h
+++ src/Entity/Component.h
@@ -51,13 +51,13 @@ struct LightComponent: public Component {
     float cutOff;
     float cutOffOuter;
 
-    LightComponent(glm::vec3&& position, glm::vec3&& ambient, glm::vec3&& defuse, glm::vec3&& specular, float constant, float linear, float quadratic)
+    LightComponent(glm::vec3&& position, glm::vec3&& ambient, glm::vec3&& diffuse, glm::vec3&& specular, float constant, float linear, float quadratic)
     :position(position), ambient(ambient), diffuse(diffuse), specular(specular), constant(constant), linear(linear), quadratic(quadratic) {};
 };
 
 struct CameraComponent: public Component {
     CameraComponent(glm::vec3 pos, unsigned index) :camIndex(index) { camera = Camera(pos); }
-    CameraComponent(Camera cam, unsigned index) : camera(camera), camIndex(index) {};
+    CameraComponent(Camera camera, unsigned index) : camera(camera), camIndex(index) {};
     CameraComponent(float x, float y, float z, unsigned index) :camIndex(index) {camera = Camera(glm::vec3(x, y, z));};
 
     Camera camera;
